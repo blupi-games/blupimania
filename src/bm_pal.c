0eb58718dbc22478d0fc47f4cd7b93a7478f2aac
Mathieu Schroeter <mathieu@schroetersa.ch>
2023-09-06 23:39:47 +0200

    Use the appropriate rank

--- src/bm_pal.c
+++ src/bm_pal.c
@@ -868,7 +868,7 @@ end:
 	if ( typepress == 0 )
 	{
 		DrawButton(GetButtonPos(press), ticon[press][tspal[press]], typer);
-		DrawF1toF4(rang);
+		DrawF1toF4(press);
 	}
 	return event == KEYCLICREL ? 1 : 0;
 }
