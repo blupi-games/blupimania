b1fa6336e8071fa43f728551d72957d084e6d18c
Mathieu Schroeter <mathieu@schroetersa.ch>
2023-11-22 23:16:42 +0100

    Fix gain to 0.2

--- misc/music/conv.sh
+++ misc/music/conv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Configure VLC to use the Windows soundfont 'GS sound set (16 bit).sf2"
-echo "Change for a gain of 0.10"
+echo "Change for a gain of 0.20"
 
 for a in *.mid; do
   cvlc "$a" --play-and-exit '--sout=#transcode{vcodec=none,acodec=flac,ab=512,channels=2,samplerate=44100}:std{access=file,mux=flac,dst=-' | ffmpeg -i pipe: -aq 0 "./${a%.mid}.ogg"
